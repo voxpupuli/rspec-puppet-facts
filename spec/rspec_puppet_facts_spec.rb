95630e03af5cf124f05e3df0239c24db36a6ca3f
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-12-19 13:28:47 +0100

    Update unit tests

--- spec/rspec_puppet_facts_spec.rb
+++ spec/rspec_puppet_facts_spec.rb
@@ -5,11 +5,15 @@ describe 'RspecPuppetFacts' do
   describe '#on_supported_os' do
 
     context 'Without parameter' do
+      subject { on_supported_os() }
 
       context 'Without metadata.json' do
         it 'should fail' do
-          expect { on_supported_os() }.to raise_error(StandardError)
+          expect { subject }.to raise_error(StandardError)
         end
+      end
+
+      context 'With a metadata.json' do
       end
     end
 
@@ -41,6 +45,35 @@ describe 'RspecPuppetFacts' do
       end
       it 'should have 4 elements' do
         expect(subject.size).to eq 4
+      end
+      it 'should return supported OS' do
+        expect(subject.keys).to eq [
+          'debian-6-x86_64',
+          'debian-7-x86_64',
+          'redhat-5-x86_64',
+          'redhat-6-x86_64',
+        ]
+      end
+    end
+
+    context 'When specifying wrong supported_os' do
+      subject {
+        on_supported_os(
+          {
+            :supported_os => [
+              {
+                "operatingsystem" => "Debian",
+                "operatingsystemrelease" => [
+                  "4",
+                ],
+              },
+            ]
+          }
+        )
+      }
+
+      it 'should fail' do
+        expect { subject }.to raise_error(StandardError, /Can't find facts for 'debian-4-x86_64'/)
       end
     end
   end
