c8818c9aa32efca99b5a8125981e5bc4f92323ce
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-12-20 19:09:56 +0100

    Don't fail if facts file not found

--- lib/rspec-puppet-facts.rb
+++ lib/rspec-puppet-facts.rb
@@ -19,13 +19,14 @@ module RspecPuppetFacts
           facter_minor_version = Facter.version[0..2]
           file = File.expand_path(File.join(File.dirname(__FILE__), "../facts/#{facter_minor_version}/#{os}.facts"))
           if ! File.file?(file)
-            fail StandardError, "Can't find facts for '#{os}' for facter #{facter_minor_version}"
+            warn "Can't find facts for '#{os}' for facter #{facter_minor_version}"
+          else
+            File.read(file).each_line do |line|
+              key, value = line.split(' => ')
+              facts[key.to_sym] = value.chomp unless value.nil?
+            end
+            h[os] = facts
           end
-          File.read(file).each_line do |line|
-            key, value = line.split(' => ')
-            facts[key.to_sym] = value.chomp unless value.nil?
-          end
-          h[os] = facts
         end
       end
     end
