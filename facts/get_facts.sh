43d1a25dc1fa2bdad3c85d9092e0749fb8110eb7
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-01-23 13:41:39 +0100

    Add facter 2.4.0 to get_facts.sh

--- facts/get_facts.sh
+++ facts/get_facts.sh
@@ -10,7 +10,7 @@ operatingsystem=$(bundle exec facter operatingsystem | tr '[:upper:]' '[:lower:]
 operatingsystemmajrelease=$(bundle exec facter operatingsystemmajrelease)
 hardwaremodel=$(bundle exec facter hardwaremodel)
 
-for version in 1.6.0 1.7.0 2.0.0 2.1.0 2.2.0 2.3.0; do
+for version in 1.6.0 1.7.0 2.0.0 2.1.0 2.2.0 2.3.0 2.4.0; do
   FACTER_GEM_VERSION="~> ${version}" bundle update
   minor_version=$(echo $version | cut -c1-3)
   output_dir="/vagrant/${minor_version}"
