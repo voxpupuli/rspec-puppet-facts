55d0b6f95cbea2e636e413aa2a22e6d441548d2c
Joshua McClintock <joshua.mcclintock1@t-mobile.com>
2015-04-02 10:25:12 -0700

    made changes to support more configureable fact locations and moved hardware to better be read from metadata.json

--- lib/rspec-puppet-facts.rb
+++ lib/rspec-puppet-facts.rb
@@ -4,7 +4,7 @@ require 'json'
 module RspecPuppetFacts
 
   def on_supported_os( opts = {} )
-    opts[:hardwaremodels] ||= ['x86_64']
+    opts[:hardwaremodels] ||= RspecPuppetFacts.meta_supported_os
     opts[:supported_os] ||= RspecPuppetFacts.meta_supported_os
     opts[:factslocation] ||= File.expand_path(File.join(File.dirname(__FILE__), "../facts/"))
 
@@ -13,16 +13,18 @@ module RspecPuppetFacts
     opts[:supported_os].map do |os_sup|
       operatingsystem = os_sup['operatingsystem'].downcase
       os_sup['operatingsystemrelease'].map do |operatingsystemmajrelease|
-        opts[:hardwaremodels].each do |hardwaremodel|
-          os = "#{operatingsystem}-#{operatingsystemmajrelease.split(" ")[0]}-#{hardwaremodel}"
-          # TODO: use SemVer here
-          facter_minor_version = Facter.version[0..2]
-          file = "#{opts[:factslocation]}/#{facter_minor_version}/#{os}.facts"
-          # Use File.exists? instead of File.file? here so that we can stub File.file?
-          if ! File.exists?(file)
-            warn "Can't find facts for '#{os}' for facter #{facter_minor_version}, skipping..."
-          else
-            h[os] = JSON.parse(IO.read(file), :symbolize_names => true)
+        opts[:hardwaremodels].map do |hardware_sup|
+          hardware_sup['hardware'].each do |hardware|
+            os = "#{operatingsystem}-#{operatingsystemmajrelease.split(" ")[0]}-#{hardware}"
+            # TODO: use SemVer here
+            facter_minor_version = Facter.version[0..2]
+            file = "#{opts[:factslocation]}/#{facter_minor_version}/#{os}.facts"
+            # Use File.exists? instead of File.file? here so that we can stub File.file?
+            if ! File.exists?(file)
+              warn "Can't find facts for '#{os}' for facter #{facter_minor_version}, skipping..."
+            else
+              h[os] = JSON.parse(IO.read(file), :symbolize_names => true)
+            end
           end
         end
       end
@@ -44,7 +46,7 @@ module RspecPuppetFacts
     end
     metadata['operatingsystem_support']
   end
-
+  
   # @api private
   def self.get_metadata
     if ! File.file?('metadata.json')
