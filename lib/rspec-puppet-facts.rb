6d951c81f1c3b9a6f051ca0044a6bbc144b06d12
Dmitry Ilyin <dilyin@mirantis.com>
2016-05-17 13:04:50 +0300

    Cleanup and refactor methods
    Make the code a little bit cleaner

--- lib/rspec-puppet-facts.rb
+++ lib/rspec-puppet-facts.rb
@@ -8,6 +8,7 @@ module RspecPuppetFacts
 
   def on_supported_os( opts = {} )
     opts[:hardwaremodels] ||= ['x86_64']
+    opts[:hardwaremodels] = [opts[:hardwaremodels]] unless opts[:hardwaremodels].is_a? Array
     opts[:supported_os] ||= RspecPuppetFacts.meta_supported_os
 
     filter = []
@@ -27,7 +28,7 @@ module RspecPuppetFacts
             filter << {
               :facterversion          => "/^#{Facter.version[0..2]}/",
               :operatingsystem        => os_sup['operatingsystem'],
-              :operatingsystemrelease => "/^#{operatingsystemmajrelease.split(" ")[0]}/",
+              :operatingsystemrelease => "/^#{operatingsystemmajrelease.split(' ')[0]}/",
               :hardwaremodel          => hardwaremodel,
             }
           end
@@ -43,39 +44,70 @@ module RspecPuppetFacts
       end
     end
 
-    h = {}
-    FacterDB::get_facts(filter).map do |facts|
-      facts.merge!({
+    received_facts = FacterDB::get_facts(filter)
+    unless received_facts.any?
+      RspecPuppetFacts.warning "No facts were found in the FacterDB for: #{filter.inspect}"
+      return {}
+    end
+
+    os_facts_hash = {}
+    received_facts.map do |facts|
+      os = "#{facts[:operatingsystem].downcase}-#{facts[:operatingsystemrelease].split('.')[0]}-#{facts[:hardwaremodel]}"
+      facts.merge! RspecPuppetFacts.common_facts
+      os_facts_hash[os] = facts
+    end
+    os_facts_hash
+  end
+
+  # These facts are common for all OS'es and will be
+  # added to the facts retrieved from the FacterDB
+  # @api private
+  # @return [Hash <Symbol => String>]
+  def self.common_facts
+    return @common_facts if @common_facts
+    @common_facts = {
         :mco_version   => MCollective::VERSION,
         :puppetversion => Puppet.version,
-        :rubysitedir   => RbConfig::CONFIG["sitelibdir"],
+        :rubysitedir   => RbConfig::CONFIG['sitelibdir'],
         :rubyversion   => RUBY_VERSION,
-      })
-      facts[:augeasversion] = Augeas.open(nil, nil, Augeas::NO_MODL_AUTOLOAD).get('/augeas/version') if Puppet.features.augeas?
-      h["#{facts[:operatingsystem].downcase}-#{facts[:operatingsystemrelease].split('.')[0]}-#{facts[:hardwaremodel]}"] = facts
+    }
+    if Puppet.features.augeas?
+      @common_facts[:augeasversion] = Augeas.open(nil, nil, Augeas::NO_MODL_AUTOLOAD).get('/augeas/version')
     end
-    h
+    @common_facts
   end
 
   # @api private
   def self.meta_supported_os
-    @meta_supported_os ||= get_meta_supported_os
-  end
-
-  # @api private
-  def self.get_meta_supported_os
-    metadata = get_metadata
-    if metadata['operatingsystem_support'].nil?
-      fail StandardError, "Unknown operatingsystem support"
+    unless metadata['operatingsystem_support'].is_a? Array
+      fail StandardError, 'Unknown operatingsystem support in the metadata file!'
     end
     metadata['operatingsystem_support']
   end
 
   # @api private
-  def self.get_metadata
-    if ! File.file?('metadata.json')
+  def self.metadata
+    return @metadata if @metadata
+    unless File.file? metadata_file
       fail StandardError, "Can't find metadata.json... dunno why"
     end
-    JSON.parse(File.read('metadata.json'))
+    content = File.read metadata_file
+    @metadata = JSON.parse content
+  end
+
+  # @api private
+  def self.metadata_file
+    'metadata.json'
+  end
+
+  # @api private
+  def self.warning(message)
+    STDERR.puts message
+  end
+
+  # @api private
+  def self.reset
+    @common_facts = nil
+    @metadata = nil
   end
 end
