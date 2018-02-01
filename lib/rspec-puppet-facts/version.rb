010538181409b159c93e265a4648dd966ae77dfb
Mickaël Canévet <mickael.canevet@camptocamp.com>
2018-02-01 08:32:15 +0100

    Release v1.9.0

--- lib/rspec-puppet-facts/version.rb
+++ lib/rspec-puppet-facts/version.rb
@@ -2,6 +2,6 @@ module RspecPuppetFacts
   # This module contains the current version constant
   module Version
     # The current version of this gem
-    STRING = '1.8.0'
+    STRING = '1.9.0'
   end
 end
