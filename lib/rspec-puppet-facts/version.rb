c855e4d59784d50f416befd61af5d83e63bd5900
Tim Sharpe <tim@bombasticmonkey.com>
2019-07-29 15:45:18 +1000

    Bump master to 2.0.0

--- lib/rspec-puppet-facts/version.rb
+++ lib/rspec-puppet-facts/version.rb
@@ -2,6 +2,6 @@ module RspecPuppetFacts
   # This module contains the current version constant
   module Version
     # The current version of this gem
-    STRING = '1.9.4'
+    STRING = '2.0.0'
   end
 end
