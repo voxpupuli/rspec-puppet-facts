1903185b0de0a55cd36b50b6c1c3bd836cea0358
Tim Sharpe <tim@bombasticmonkey.com>
2019-12-11 21:03:41 +1100

    Prepare 1.10.0 release

--- lib/rspec-puppet-facts/version.rb
+++ lib/rspec-puppet-facts/version.rb
@@ -2,6 +2,6 @@ module RspecPuppetFacts
   # This module contains the current version constant
   module Version
     # The current version of this gem
-    STRING = '1.9.6'
+    STRING = '1.10.0'
   end
 end
