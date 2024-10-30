8d487536c47e675f188bf163690da4cc2313d667
Tim Meusel <tim@bastelfreak.de>
2024-10-30 11:22:14 +0100

    Release 5.2.0

--- lib/rspec-puppet-facts/version.rb
+++ lib/rspec-puppet-facts/version.rb
@@ -2,6 +2,6 @@ module RspecPuppetFacts
   # This module contains the current version constant
   module Version
     # The current version of this gem
-    STRING = '5.1.0'
+    STRING = '5.2.0'
   end
 end
